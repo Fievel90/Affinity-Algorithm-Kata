8a7a98dac3fd7c3b1c44de1a28163fe2eb53daa8
Mario Buonomo <mario.buonomo@facile.it>
2020-05-25 12:20:24 +0200

    feat: removed 2 for

--- typescript/src/Affinity/Affinity.ts
+++ typescript/src/Affinity/Affinity.ts
@@ -1,59 +1,54 @@
 export default class Affinity {
-    private readonly people : string[];
-    private readonly affinities : {} | any;
+  private readonly people: string[];
+  private readonly affinities: {} | any;
 
-    constructor(people: string[], affinities: {}) {
-        this.people = people;
-        this.affinities = affinities;
-    }
+  constructor(people: string[], affinities: {}) {
+    this.people = people;
+    this.affinities = affinities;
+  }
 
-    public getGroups(): string[][] {
-        const num : number = this.people.length;
-        const groups : string[][] = [];
+  // const people : string[] = ['Adah', 'Tianna', 'Sandy', 'Zora'];
+  // const affinities = {
+  //     'Adah': {'Adah' : false, 'Tianna' : true, 'Sandy' : true, 'Zora': true},
+  //     'Tianna': {'Adah' : true, 'Tianna' : false, 'Sandy' : true, 'Zora': true},
+  //     'Sandy': {'Adah' : true, 'Tianna' : true, 'Sandy' : false, 'Zora': true},
+  //     'Zora': {'Adah' : true, 'Tianna' : false, 'Sandy' : true, 'Zora' :false},
+  // };
 
+  public getGroups(): string[][] {
+    const num: number = this.people.length;
+    const groups: string[][] = [];
+
+    // tslint:disable-next-line:no-bitwise
+    for (let i: number = 0; i < 1 << num; ++i) {
+      const group: string[] = [];
+      let toExclude: boolean = false;
+
+      for (let j: number = 0; j < num; ++j) {
         // tslint:disable-next-line:no-bitwise
-        for (let i : number = 0; i < (1 << num); ++i) {
-            const group : string[] = [];
-            let toExclude : boolean = false;
+        if ((1 << j) & i) {
+          group.push(this.people[j]);
 
-            for (let j : number = 0; j < num; ++j) {
-                // tslint:disable-next-line:no-bitwise
-                if ((1 << j) & i) {
-                    group.push(this.people[j]);
+          group.forEach((first) => {
+            group
+              .filter((n) => n != first)
+              .forEach((second) => {
+                let check: boolean = this.affinities[first][second] ?? false;
+                let iCheck: boolean = this.affinities[second][first] ?? false;
 
-                    for (let x : number = 0; x < group.length; ++x) {
-                        for (let y : number = x + 1; y < group.length; ++y) {
-                            const first : string = group[x];
-                            const second : string = group[y];
-
-                            let check : boolean = false;
-                            let iCheck: boolean = false;
-
-                            if (this.affinities.hasOwnProperty(first)
-                                && this.affinities[first].hasOwnProperty(second)
-                            ) {
-                                check = this.affinities[first][second] ?? false;
-                            }
-
-                            if (this.affinities.hasOwnProperty(second)
-                                && this.affinities[second].hasOwnProperty(first)
-                            ) {
-                                iCheck = this.affinities[second][first] ?? false;
-                            }
-
-                            if (!check || !iCheck) {
-                                toExclude = true;
-                            }
-                        }
-                    }
+                if (!check || !iCheck) {
+                  toExclude = true;
                 }
-            }
+              });
+          });
+        } // fine 33
+      } // fine 31
 
-            if (group.length > 1 && !toExclude) {
-                groups.push(group);
-            }
-        }
-
-        return groups;
+      if (group.length > 1 && !toExclude) {
+        groups.push(group);
+      }
     }
+
+    return groups;
+  }
 }
